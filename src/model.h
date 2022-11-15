4a3be18e455e2345c665e50963bc7fa46a74540e
zouchanglin <zouchanglin@tal.com>
2022-11-15 18:24:55 +0800

    add: 纹理贴图的实现（纹理坐标插值）

--- src/model.h
+++ src/model.h
@@ -1,17 +1,35 @@
-#pragma once
-
+#ifndef __MODEL_H__
+#define __MODEL_H__
 #include <vector>
+#include <string>
 #include "geometry.h"
+#include "tgaimage.h"
 
-class Model {
+class Model
+{
 private:
-	std::vector<Vec3f> verts_;
-	std::vector<std::vector<int> > faces_;
+    std::vector<Vec3f> verts_;
+    std::vector<std::vector<Vec3i> > faces_; // attention, this Vec3i means vertex/uv/normal
+
+    std::vector<Vec3f> norms_;
+    std::vector<Vec2f> uv_;
+    TGAImage diffusemap_;
+    TGAImage normalmap_;
+    TGAImage specularmap_;
+    void load_texture(std::string filename, TGAImage &img);
 public:
-	explicit Model(const char *filename);
-	~Model();
-	int nverts();
-	int nfaces();
-	Vec3f vert(int i);
-	std::vector<int> face(int idx);
+    Model(const char *obj_path, const char *diffuse_path, const char *nm_path, const char *spec_path);
+    Model(const char *obj_path);
+    ~Model();
+    int nverts();
+    int nfaces();
+    Vec3f normal(int iface, int nthvert);
+    Vec3f normal(Vec2f uv);
+    Vec3f vert(int i);
+    Vec3f vert(int iface, int nthvert);
+    Vec2f uv(int iface, int nthvert);
+    TGAColor diffuse(Vec2f uv);
+    float specular(Vec2f uv);
+    std::vector<int> face(int idx);
 };
+#endif //__MODEL_H__
